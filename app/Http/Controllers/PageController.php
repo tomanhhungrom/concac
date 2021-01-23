175defef5145c42b6aea666a738fceebcc9e5c1f
tomanhhungrom <phanvanhoathuy1806@gmail.com>
2021-01-23 20:35:46 +0700

    buom

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\ProductType;
+use App\Slide;
+use Illuminate\Http\Request;
+use App\Cart;
+use Illuminate\Contracts\Session\Session as SessionSession;
+use Illuminate\Support\Facades\Session;
+use App\Http\Requests;
+use App\Customer;
+use App\Bill;
+use App\BillDetail;
+
+// use App\Cart;
+// use App\Models\Product;
+// use App\Models\ProductType;
+// use App\Models\Slide;
+use App\User;
+// use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Session;
+
+class PageController extends Controller
+{
+    public function getIndex(){
+    	$slide = Slide::all();
+    	// print_r($slide);
+    	// exit;
+        // return view('page.trangchu',['slide'=>$slide]);
+        $new_product = Product::where('new',1)->paginate(4);
+        $sp_khuyenmai = Product::where('promotion_price','<>',0)->paginate(8);
+    	return view('page.trangchu', compact('slide','new_product','sp_khuyenmai'));
+    }
+
+    function getLoaisp($type){
+        $sp_theoloai = Product::where('id_type', $type)->get();
+        $sp_khac = Product::where('id_type', '<>', $type)->paginate(3);
+        $loai = ProductType::all();
+        $loai_sp = ProductType::where('id', $type)->first();
+        return view('page.loai_sanpham', compact('sp_theoloai', 'sp_khac', 'loai', 'loai_sp'));
+    }
+
+    public function getChitiet( Request $req){
+        $sanpham = Product::where('id',$req->id)->first();
+        $sp_tuongtu = Product::where('id_type',$sanpham->id_type)->paginate(6);
+    	return view('page.chitiet_sanpham',compact('sanpham','sp_tuongtu'));
+    }
+    public function getLienhe(){
+    	return view('page.lienhe');
+    }
+    public function getGioithieu(){
+    	return view('page.gioithieu');
+    }
+    public function getAddtoCart(Request $req, $id){
+        $product = Product::find($id);
+        $oldCart = Session('cart')?Session::get('cart'):null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $id);
+        $req->session()->put('cart', $cart);
+        return redirect()->back();
+    }
+    public function getDelItemCart($id){
+        $oldCart = Session::has('cart')?Session::get('cart'):null;
+        $cart = new Cart($oldCart);
+        $cart->removeItem($id);
+        if(count($cart->items)>0){
+            Session::put('cart',$cart);
+        }
+        else{
+            Session::forget('cart');
+        }
+        Session::put('cart',$cart);
+        return redirect()->back();
+    }
+    public function getCheckout(){
+        return view('page.dat_hang');
+    }
+    public function postChekout(Request $req){
+        $cart = Session::get('cart');
+        $customer = new Customer;
+        $customer->name = $req->name;
+        $customer->gender = $req->name;
+        $customer->email = $req->email;
+        $customer->addres = $req->address;
+        $customer->phone_number = $req->phone;
+        $customer->note = $req->notes;
+        $customer->save();
+
+        $bill = new Bill;
+        $bill->id_customer = $customer->id;
+        $bill->date_order = date('Y-m-d');
+        $bill->total = $cart->totalPrice;
+        $bill->payment = $req->payment;
+        $bill->note = $req->notes;
+        $bill->save();
+
+        foreach ($cart['item'] as $key => $value) {
+            $bill_detail = new BillDetail;
+            $bill_detail->id_bill = $bill->id;
+            $bill_detail->id_product = $key;
+            $bill_detail->quantity = $value['qty'];
+            $bill_detail->unit_price = $value['price']/$value['qty'];
+            $bill_detail->save();
+
+            
+        }
+        Session::forget('cart');
+        return redirect()->back()->with('thongbao','Đặt Hàng Thành Công');
+        
+
+    }
+}
---
+++ resources/views/page/chitiet_sanpham.blade.php
@@ -0,0 +1,192 @@
+@extends('master')
+@section('content')
+	<div class="inner-header">
+		<div class="container">
+			<div class="pull-left">
+				<h6 class="inner-title">Sản Phẩm {{$sanpham->name}}</h6>
+			</div>
+			<div class="pull-right">
+				<div class="beta-breadcrumb font-large">
+					<a href="{{route('trang-chu')}}">Trang Chủ</a> / <span>Thông tin chi tiết sản phẩm</span>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div id="content">
+			<div class="row">
+				<div class="col-sm-9">
+
+					<div class="row">
+						<div class="col-sm-4">
+							<img src="image/product/pizza.jpg" alt="">
+						</div>
+						<div class="col-sm-8">
+							<div class="single-item-body">
+								<p class="single-item-title"><h2>{{$sanpham->name}}</h2></p>
+								<p class="single-item-price">
+										@if ($sanpham->promotion_price == 0)
+												<span class="flash-sale">{{ number_format($sanpham->unit_price) }} đồng</span>
+												@else
+												<span class="flash-del">{{ number_format($sanpham->unit_price) }} đồng</span>
+												<span class="flash-sale">{{ number_format($sanpham->promotion_price) }} đồng</span>
+												@endif
+								</p>
+							</div>
+
+							<div class="clearfix"></div>
+							<div class="space20">&nbsp;</div>
+
+							<div class="single-item-desc">
+								<p>{{$sanpham->descripsion}}</p>
+							</div>
+							<div class="space20">&nbsp;</div>
+
+							<p>Số Lượng:</p>
+							<div class="single-item-options">
+
+							<select class="wc-select" name="color">
+                                <option>Số lượng</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+								
+								<a class="add-to-cart" href="#"><i class="fa fa-shopping-cart"></i></a>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+					</div>
+
+					<div class="space40">&nbsp;</div>
+					<div class="woocommerce-tabs">
+						<ul class="tabs">
+							<li><a href="#tab-description">Mô Tả</a></li>
+							<!-- <li><a href="#tab-reviews">Reviews (0)</a></li> -->
+						</ul>
+
+						<div class="panel" id="tab-description">
+							<p>{{$sanpham->descripsion}}</p>
+							<!-- <p>Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequaturuis autem vel eum iure reprehenderit qui in ea voluptate velit es quam nihil molestiae consequr, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p> -->
+						</div>
+						<!-- <div class="panel" id="tab-reviews">
+							<p>No Reviews</p>
+						</div> -->
+					</div>
+					<div class="space50">&nbsp;</div>
+					<div class="beta-products-list">
+						<h4>Sản Phẩm Tương Tự</h4>
+
+						<div class="row">
+						@foreach($sp_tuongtu as $sptt)
+							<div class="col-sm-4">
+								<div class="single-item">
+									@if ($sptt->promotion_price != 0)
+                                    <div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+                                    @endif
+									<div class="single-item-header">
+										<a href="product.html"><img src="images/products/{{$sptt->image}}" alt="" height="150px"></a>
+									</div>
+									<div class="single-item-body">
+										<p class="single-item-title">{{$sptt->name}}</p>
+										<p class="single-item-price" style="font-size: 18px;">
+										@if ($sptt->promotion_price == 0)
+                                            <span class="flash-sale">{{ number_format($sptt->unit_price) }} đồng</span>
+                                            @else
+                                            <span class="flash-del">{{ number_format($sptt->unit_price) }} đồng</span>
+                                            <span class="flash-sale">{{ number_format($sptt->promotion_price) }} đồng</span>
+                                            @endif
+										</p>
+									</div>
+									<div class="single-item-caption">
+										<a class="add-to-cart pull-left" href="product.html"><i class="fa fa-shopping-cart"></i></a>
+										<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+										<div class="clearfix"></div>
+									</div>
+								</div>
+							</div>
+						@endforeach
+						<div class="row">{{$sp_tuongtu->links()}}</div>
+						</div>
+					</div> <!-- .beta-products-list -->
+				</div>
+				<div class="col-sm-3 aside">
+					<div class="widget">
+						<h3 class="widget-title">Best Sellers</h3>
+						<div class="widget-body">
+							<div class="beta-sales beta-lists">
+								<div class="media beta-sales-item">
+									<a class="pull-left" href="product.html"><img src="assets/dest/images/products/sales/1.png" alt=""></a>
+									<div class="media-body">
+										Sample Woman Top
+										<span class="beta-sales-price">$34.55</span>
+									</div>
+								</div>
+								<div class="media beta-sales-item">
+									<a class="pull-left" href="product.html"><img src="assets/dest/images/products/sales/2.png" alt=""></a>
+									<div class="media-body">
+										Sample Woman Top
+										<span class="beta-sales-price">$34.55</span>
+									</div>
+								</div>
+								<div class="media beta-sales-item">
+									<a class="pull-left" href="product.html"><img src="assets/dest/images/products/sales/3.png" alt=""></a>
+									<div class="media-body">
+										Sample Woman Top
+										<span class="beta-sales-price">$34.55</span>
+									</div>
+								</div>
+								<div class="media beta-sales-item">
+									<a class="pull-left" href="product.html"><img src="assets/dest/images/products/sales/4.png" alt=""></a>
+									<div class="media-body">
+										Sample Woman Top
+										<span class="beta-sales-price">$34.55</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div> <!-- best sellers widget -->
+					<div class="widget">
+						<h3 class="widget-title">New Products</h3>
+						<div class="widget-body">
+							<div class="beta-sales beta-lists">
+								<div class="media beta-sales-item">
+									<a class="pull-left" href="product.html"><img src="assets/dest/images/products/sales/1.png" alt=""></a>
+									<div class="media-body">
+										Sample Woman Top
+										<span class="beta-sales-price">$34.55</span>
+									</div>
+								</div>
+								<div class="media beta-sales-item">
+									<a class="pull-left" href="product.html"><img src="assets/dest/images/products/sales/2.png" alt=""></a>
+									<div class="media-body">
+										Sample Woman Top
+										<span class="beta-sales-price">$34.55</span>
+									</div>
+								</div>
+								<div class="media beta-sales-item">
+									<a class="pull-left" href="product.html"><img src="assets/dest/images/products/sales/3.png" alt=""></a>
+									<div class="media-body">
+										Sample Woman Top
+										<span class="beta-sales-price">$34.55</span>
+									</div>
+								</div>
+								<div class="media beta-sales-item">
+									<a class="pull-left" href="product.html"><img src="assets/dest/images/products/sales/4.png" alt=""></a>
+									<div class="media-body">
+										Sample Woman Top
+										<span class="beta-sales-price">$34.55</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div> <!-- best sellers widget -->
+				</div>
+			</div>
+		</div> <!-- #content -->
+	</div> <!-- .container -->
+@endsection
